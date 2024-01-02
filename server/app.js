67b1739f04b0c851533264396e37905851263644
mray1111 <manishcode111@gmail.com>
2024-01-02 22:34:51 +0530

    Finally Done RazorpayTEDx for testing

--- server/app.js
+++ server/app.js
@@ -10,7 +10,3 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", paymentRoute);
-
-app.get("/api/getkey", (req, res) =>
-  res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
-);
